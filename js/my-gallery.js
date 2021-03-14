fc357480d08e6cf15d4b4d05088eb484247b6714
Irina Murashka <irvil@tut.by>
2021-03-14 15:57:36 +0000

    открытие и закрытие модалки

--- js/my-gallery.js
+++ js/my-gallery.js
@@ -1,6 +1,10 @@
 import gallery from './gallery-items.js';
 
 const ulRef = document.querySelector('.js-gallery');
+const modal = document.querySelector('.js-lightbox');
+const openModalImg = document.querySelector('.lightbox__image');
+const modalCloseBtn = document.querySelector('button[data-action="close-lightbox"]'), //кнопка закрытия модального окна
+
 
 // Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
@@ -34,4 +38,38 @@ const elements = imgMarkup(gallery);
 
 ulRef.append(...elements);
 
-console.log(ulRef);
+// console.log(ulRef);
+
+
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Подмена значения атрибута src элемента img.lightbox__image
+
+ulRef.addEventListener('click', onOpenModal);
+
+function onOpenModal(evt) {
+    
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    evt.preventDefault(); //отмена перехода по ссылке
+    
+    modal.classList.add('is-open'); //открытие модалки по клику
+    openModalImg.src = evt.target.dataset.source; //Подмена значения атрибута src элемента img.lightbox__image
+};
+
+
+// Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
+
+modalCloseBtn.addEventListener('click', onCloseModal);
+
+function onCloseModal(event) {
+    if (event.currentTarget === event.target) {
+        onCloseModal();
+    }
+};
+
+function onCloseModal() {
+    modal.classList.remove('is-open');
+    openModalImg.src = '';   //очстка от атрибута
+
+}
